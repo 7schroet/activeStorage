30fcb157f9759776ff3bda02f50dab24b73b2ad9
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-06-22 16:19:51 +0200

    use rpc lib

--- server/main.c
+++ server/main.c
@@ -17,53 +17,15 @@
  * limitations under the License.
  */
 
-#include <mercury.h>
-#include <stdio.h>
+#include "rpc.h"
 #include <stdlib.h>
-
-#define HG_RETURN_CHECK(func)                                                  \
-  do                                                                           \
-  {                                                                            \
-    hg_return_t ret = (func);                                                  \
-    if (ret != HG_SUCCESS)                                                     \
-    {                                                                          \
-      fprintf(stderr, "Failure in HG function in file %s at line %d\n",        \
-              __FILE__, __LINE__);                                             \
-      fprintf(stderr, "Function call: %s\n", #func);                           \
-      exit(EXIT_FAILURE);                                                      \
-    }                                                                          \
-  } while (0)
-
-void setup(hg_class_t** class, hg_context_t** context)
-{
-  *class = HG_Init("tcp://:12345", HG_TRUE);
-  *context = HG_Context_create(*class);
-}
-
-void print_address(hg_class_t* class)
-{
-  hg_addr_t addr;
-  HG_RETURN_CHECK(HG_Addr_self(class, &addr));
-
-  hg_size_t buf_size = 128;
-  char buf[buf_size];
-  HG_RETURN_CHECK(HG_Addr_to_string(class, buf, &buf_size, addr));
-
-  printf("Server address is: %s\n", buf);
-
-  HG_RETURN_CHECK(HG_Addr_free(class, addr));
-}
 
 int main(void)
 {
-  hg_class_t* class = NULL;
-  hg_context_t* context = NULL;
-
-  setup(&class, &context);
-  print_address(class);
-
-  HG_RETURN_CHECK(HG_Context_destroy(context));
-  HG_RETURN_CHECK(HG_Finalize(class));
+  rpc_handle* handle = NULL;
+  rpc_init(&handle);
+  rpc_address_to_file(handle);
+  rpc_finalize(handle);
 
   return EXIT_SUCCESS;
 }
