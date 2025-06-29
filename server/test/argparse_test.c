afbd194d5cd6c6914d0eeb321d46606b08d3dad1
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-06-30 00:00:43 +0200

    add more parse tests

--- server/test/argparse_test.c
+++ server/test/argparse_test.c
@@ -27,12 +27,120 @@
 
 #include "../argparse.c"
 
-static void null_test(void** state) { (void)state; }
+static int reset_getopt(void** state)
+{
+  optind = 1;
+  return 0;
+}
+
+static void assert_config_equal(config* expected, config* actual)
+{
+  assert_string_equal(expected->port, actual->port);
+  assert_string_equal(expected->address_file, actual->address_file);
+  assert_int_equal(expected->protocol, actual->protocol);
+}
+
+static void default_test(void** state)
+{
+  config expected = {
+      .address_file = "servername",
+      .port = "8080",
+      .protocol = tcp,
+  };
+
+  char* argv[1] = {"as-server"};
+  config actual = parse_args(1, argv);
+  assert_config_equal(&expected, &actual);
+}
+
+static void pass_port_test(void** state)
+{
+  config expected = {
+      .address_file = "servername",
+      .port = "12345",
+      .protocol = tcp,
+  };
+
+  char* argv[3] = {"as-server", "--port", "12345"};
+  config actual = parse_args(3, argv);
+  assert_config_equal(&expected, &actual);
+}
+
+static void pass_protocol_test(void** state)
+{
+  config expected = {
+      .address_file = "servername",
+      .port = "8080",
+      .protocol = verbs,
+  };
+
+  char* argv[3] = {"as-server", "--protocol", "verbs"};
+  config actual = parse_args(3, argv);
+  assert_config_equal(&expected, &actual);
+}
+
+static void pass_address_file_test(void** state)
+{
+  config expected = {
+      .address_file = "../filename",
+      .port = "8080",
+      .protocol = tcp,
+  };
+
+  char* argv[3] = {"as-server", "--addressfile", "../filename"};
+  config actual = parse_args(3, argv);
+  assert_config_equal(&expected, &actual);
+}
+
+static void pass_all_test(void** state)
+{
+  config expected = {
+      .address_file = "file",
+      .port = "6666",
+      .protocol = tcp,
+  };
+
+  char* argv[7] = {"as-server", "--port",        "6666", "--protocol",
+                   "tcp",       "--addressfile", "file"};
+  config actual = parse_args(7, argv);
+  assert_config_equal(&expected, &actual);
+}
+
+void __real_exit(int);
+
+void __wrap_exit(int exit_code)
+{
+  function_called();
+  /*__real_exit(0);*/
+}
+
+static void missing_arg_test(void** state)
+{
+  expect_function_call(__wrap_exit);
+
+  char* argv[6] = {"as-server",  "--port", "6666",
+                   "--protocol", "tcp",    "--addressfile"};
+  config actual = parse_args(6, argv);
+}
+
+static void pass_help_test(void** state)
+{
+  expect_function_call(__wrap_exit);
+
+  char* argv[2] = {"as-server", "--help"};
+  config actual = parse_args(2, argv);
+}
 
 int main(void)
 {
   const struct CMUnitTest tests[] = {
-      cmocka_unit_test(null_test),
+      cmocka_unit_test_setup(default_test, reset_getopt),
+      cmocka_unit_test_setup(pass_port_test, reset_getopt),
+      cmocka_unit_test_setup(pass_protocol_test, reset_getopt),
+      cmocka_unit_test_setup(pass_address_file_test, reset_getopt),
+      cmocka_unit_test_setup(pass_all_test, reset_getopt),
+      cmocka_unit_test_setup(missing_arg_test, reset_getopt),
+      cmocka_unit_test_setup(pass_help_test, reset_getopt),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
