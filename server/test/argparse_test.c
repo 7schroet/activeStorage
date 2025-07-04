bb05ac884641f13d31deb0efd0473e9bc182828c
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-07-04 13:16:26 +0200

    use tap format

--- server/test/argparse_test.c
+++ server/test/argparse_test.c
@@ -152,8 +152,9 @@ int main(void)
   Suite* parse = parser_suite();
 
   SRunner* sr = srunner_create(parse);
+  srunner_set_tap(sr, "-");
 
-  srunner_run_all(sr, CK_NORMAL);
+  srunner_run_all(sr, CK_SILENT);
   int number_failed = srunner_ntests_failed(sr);
   srunner_free(sr);
   return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
