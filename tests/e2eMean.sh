e311342f879a6669b74b12c7fd1dacc982e79daf
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-09-24 11:22:47 +0200

    clean up input h5 file

--- tests/e2eMean.sh
+++ tests/e2eMean.sh
@@ -4,6 +4,7 @@ set -ue
 
 ADDRESS_FILE=e2eMeanAddress
 PORT=8080
+H5FILE=file.h5
 RESULT=asrpc_results_file__dataset_mean.h5
 
 $1 --addressfile $ADDRESS_FILE --port $PORT &
@@ -14,5 +15,5 @@ sleep 1
 (for _ in $(seq 1 20); do echo ; done) | $2 --addressfile $ADDRESS_FILE
 
 h5dump $RESULT
-rm $RESULT $ADDRESS_FILE
+rm $RESULT $ADDRESS_FILE $H5FILE
 kill $PID
