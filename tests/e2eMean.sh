920e3717b660adc9105242144c125058bac423d2
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-09-24 13:28:25 +0200

    add reference file for e2e mean test

--- tests/e2eMean.sh
+++ tests/e2eMean.sh
@@ -6,6 +6,7 @@ ADDRESS_FILE=e2eMeanAddress
 PORT=8080
 H5FILE=file.h5
 RESULT=asrpc_results_file__dataset_mean.h5
+RESULT_REF=$3
 
 $1 --addressfile $ADDRESS_FILE --port $PORT &
 PID=$!
@@ -14,6 +15,10 @@ sleep 1
 
 (for _ in $(seq 1 20); do echo ; done) | $2 --addressfile $ADDRESS_FILE
 
-h5dump $RESULT
-rm $RESULT $ADDRESS_FILE $H5FILE
-kill $PID
+function cleanup(){
+  rm "$RESULT" "$ADDRESS_FILE" "$H5FILE"
+  kill $PID
+}
+trap cleanup EXIT
+
+h5diff "$RESULT" "$RESULT_REF"
