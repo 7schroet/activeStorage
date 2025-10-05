60b9028480b852b65168b0a55d43f78755c2c1bc
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-10-05 22:31:11 +0200

    refactor mean reduction

--- rpc/include/as_rpc_kernel_math.hpp
+++ rpc/include/as_rpc_kernel_math.hpp
@@ -40,18 +40,17 @@ std::vector<double> mean_reduction(const std::vector<T>& data,
   assert((dims.size() == reduce_along_dim.size()) &&
          "Dimensionalities do not match!");
 
-  std::vector<hsize_t> new_dims{dims};
   const auto num_reduced_dims =
       std::reduce(reduce_along_dim.begin(), reduce_along_dim.end(), 0u);
   const bool reduce_to_single_value = num_reduced_dims == dims.size();
-
-  if (dims.size() == 1 || reduce_to_single_value)
+  if (reduce_to_single_value)
   {
     const double sum = std::reduce(data.begin(), data.end(), 0.0);
     const auto avg = sum / data.size();
     return {avg};
   }
 
+  std::vector<hsize_t> new_dims{dims};
   std::vector<double> result(data.begin(), data.end());
 
   for (int current_dim = new_dims.size() - 1; current_dim >= 0; current_dim--)
@@ -59,25 +58,26 @@ std::vector<double> mean_reduction(const std::vector<T>& data,
     if (!(reduce_along_dim[current_dim]))
       continue;
 
-    std::vector<double> tmp{};
-    const auto stride_between_elements =
+    const auto elements_post_reduction = result.size() / new_dims[current_dim];
+    std::vector<double> tmp(elements_post_reduction);
+
+    const auto stride_for_reduction =
         std::reduce(new_dims.begin() + current_dim + 1, new_dims.end(), 1ul,
                     std::multiplies<>());
 
-    const auto reduced_num_elements = result.size() / new_dims[current_dim];
     const auto stride_between_starts =
-        stride_between_elements == 1 ? new_dims[current_dim] : 1ul;
+        stride_for_reduction == 1 ? new_dims[current_dim] : 1ul;
 
-    for (auto i = 0ul; i < reduced_num_elements; i++)
+    for (auto i = 0ul; i < elements_post_reduction; i++)
     {
       double sum = 0.0;
       auto offset = i * stride_between_starts;
       for (auto count = 0ul; count < new_dims[current_dim]; count++)
       {
         sum += result[offset];
-        offset += stride_between_elements;
+        offset += stride_for_reduction;
       }
-      tmp.push_back(sum / new_dims[current_dim]);
+      tmp[i] = sum / new_dims[current_dim];
     }
 
     new_dims[current_dim] = 1;
