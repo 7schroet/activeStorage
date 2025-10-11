c97bd5f92afc3a6dfae403c23472b7b0a086f2d5
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-10-12 00:03:31 +0200

    introduce clang-tidy into static analysis

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -1,5 +1,5 @@
 find_program(CPPCHECK_EXE cppcheck REQUIRED)
-add_custom_target(analysis
+add_custom_target(cppcheck
   COMMAND ${CPPCHECK_EXE}
     --project=${CMAKE_BINARY_DIR}/compile_commands.json
     -itest
@@ -10,4 +10,12 @@ add_custom_target(analysis
     --error-exitcode=3
     -j 4
     --suppressions-list=${CMAKE_SOURCE_DIR}/cppcheck_suppress.txt
+)
+
+find_program(CLANG_TIDY_EXE run-clang-tidy)
+add_custom_target(clang-tidy
+  COMMAND ${CLANG_TIDY_EXE}
+    -config=
+    -p ${CMAKE_BINARY_DIR}
+    -allow-no-checks
 )
