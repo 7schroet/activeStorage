6f47a77fb8fb4141d19d20b520e2adfe712f5d08
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-10-12 13:50:56 +0200

    remove cppcheck suppressions

--- cmake/static_analysis.cmake
+++ cmake/static_analysis.cmake
@@ -1,4 +1,6 @@
 find_program(CPPCHECK_EXE cppcheck REQUIRED)
+set(cppcheck_dir ${CMAKE_BINARY_DIR}/cppcheck)
+file(MAKE_DIRECTORY ${cppcheck_dir})
 add_custom_target(cppcheck
   COMMAND ${CPPCHECK_EXE}
     --project=${CMAKE_BINARY_DIR}/compile_commands.json
@@ -8,8 +10,8 @@ add_custom_target(cppcheck
     --check-level=exhaustive
     --inconclusive
     --error-exitcode=3
-    -j 4
-    --suppressions-list=${CMAKE_SOURCE_DIR}/cppcheck_suppress.txt
+    -j 4 --std=c++23
+    --cppcheck-build-dir=${cppcheck_dir}
 )
 
 find_program(CLANG_TIDY_EXE run-clang-tidy)
