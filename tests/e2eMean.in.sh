7b28486e8fe59d4462d91246c50bfc641486892b
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-10-13 14:06:01 +0200

    refactor e2e test

--- tests/e2eMean.in.sh
+++ tests/e2eMean.in.sh
@@ -5,13 +5,16 @@
 
 set -ue
 
+SERVER_EXE=$1
+CLIENT_EXE=$2
+
 ADDRESS_FILE=e2eMeanAddress
 PORT=8080
 H5FILE=file.h5
 RESULT=asrpc_results_file__dataset_mean.h5
 MEAN_INPUTS=("001" "010" "011" "100" "101" "110" "111")
 
-$1 --addressfile $ADDRESS_FILE --port $PORT &
+$SERVER_EXE --addressfile $ADDRESS_FILE --port $PORT &
 PID=$!
 
 sleep 1
@@ -22,10 +25,12 @@ function cleanup(){
 }
 trap cleanup EXIT
 
-for input in "${MEAN_INPUTS[@]}"; do
+function run_test_case(){
+  local mean_in=$1
+  local type_in=$2
 
-  RESULT_REF="@CMAKE_CURRENT_SOURCE_DIR@/e2eMean${input}.h5"
-  (for _ in $(seq 1 20); do echo ; done) | $2 --addressfile $ADDRESS_FILE --mean "$input" --type double
+  local result_ref="@CMAKE_CURRENT_SOURCE_DIR@/e2eMean${input}.h5"
+  (for _ in $(seq 1 20); do echo ; done) | $CLIENT_EXE --addressfile $ADDRESS_FILE --mean "$mean_in" --type "$type_in"
 
   # h5diff is somewhat inconsistent. If the files can be diffed and there are
   # no differences, the exit code is 0 and there is not stdout. If there are
@@ -34,38 +39,25 @@ for input in "${MEAN_INPUTS[@]}"; do
   # is still 0. This case if obviously a failure. So for that case, we need to also
   # capture stdout and check if it is empty, alongside the exit code check.
   set +e
-  h5diff_result=$(@HDF5_DIFF_EXECUTABLE@ -c "$RESULT" "$RESULT_REF")
+  h5diff_result=$(@HDF5_DIFF_EXECUTABLE@ -c "$RESULT" "$result_ref")
   if [[ "$h5diff_result" || $? != 0 ]]; then
-    echo "Failure while comparing to $RESULT_REF:"
+    echo "Failure while comparing to $result_ref:"
     echo "$h5diff_result"
 
-    COPY_ON_FAIL="./failure.h5"
-    echo "Copying failed file to $(realpath $COPY_ON_FAIL)"
-    cp "$RESULT" "$COPY_ON_FAIL"
+    local copy_on_fail="./failure.h5"
+    echo "Copying failed file to $(realpath $copy_on_fail)"
+    cp "$RESULT" "$copy_on_fail"
 
     exit 1
   fi
   set -e
   rm "$RESULT"
+}
+
+for input in "${MEAN_INPUTS[@]}"; do
+  run_test_case "$input" "double"
 done
 
 for input in "float" "int"; do
-
-  RESULT_REF="@CMAKE_CURRENT_SOURCE_DIR@/e2eMeanType${input}.h5"
-  (for _ in $(seq 1 20); do echo ; done) | $2 --addressfile $ADDRESS_FILE --mean "011" --type "$input"
-
-  set +e
-  h5diff_result=$(@HDF5_DIFF_EXECUTABLE@ -c "$RESULT" "$RESULT_REF")
-  if [[ "$h5diff_result" || $? != 0 ]]; then
-    echo "Failure while comparing to $RESULT_REF:"
-    echo "$h5diff_result"
-
-    COPY_ON_FAIL="./failure.h5"
-    echo "Copying failed file to $(realpath $COPY_ON_FAIL)"
-    cp "$RESULT" "$COPY_ON_FAIL"
-
-    exit 1
-  fi
-  set -e
-  rm "$RESULT"
+  run_test_case "011" "$input"
 done
