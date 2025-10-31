3713073c255d6fadccb9842b8e5a7c9f1464c580
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-10-31 21:13:54 +0100

    fix cppcheck violations

--- plugin/components/wrap.cpp
+++ plugin/components/wrap.cpp
@@ -28,7 +28,7 @@ namespace
 H5VL_as_rpc_t* H5VL_as_rpc_new_obj(void* under_obj, hid_t under_vol_id)
 {
   H5VL_as_rpc_t* new_obj;
-  new_obj = (H5VL_as_rpc_t*)malloc(sizeof(H5VL_as_rpc_t));
+  new_obj = new H5VL_as_rpc_t;
   new_obj->under_object = under_obj;
   new_obj->under_vol_id = under_vol_id;
   H5Iinc_ref(under_vol_id);
@@ -41,7 +41,7 @@ herr_t H5VL_as_rpc_free_obj(H5VL_as_rpc_t* obj)
   err = H5Eget_current_stack();
   H5Idec_ref(obj->under_vol_id);
   H5Eset_current_stack(err);
-  free(obj);
+  delete obj;
   return 0;
 }
 
@@ -49,17 +49,16 @@ herr_t H5VL_as_rpc_free_obj(H5VL_as_rpc_t* obj)
 
 void* H5VL_as_rpc_get_object(const void* obj)
 {
-  auto o = (const H5VL_as_rpc_t*)obj;
+  auto o = static_cast<const H5VL_as_rpc_t*>(obj);
   return H5VLget_object(o->under_object, o->under_vol_id);
 }
 
 herr_t H5VL_as_rpc_get_wrap_ctx(const void* obj, void** wrap_ctx)
 {
-  auto o = (const H5VL_as_rpc_t*)obj;
+  auto o = static_cast<const H5VL_as_rpc_t*>(obj);
   H5VL_as_rpc_ctx_t* new_wrap_ctx;
 
-  new_wrap_ctx = (H5VL_as_rpc_ctx_t*)malloc(sizeof(H5VL_as_rpc_ctx_t));
-
+  new_wrap_ctx = new H5VL_as_rpc_ctx_t;
   new_wrap_ctx->under_vol_id = o->under_vol_id;
   H5Iinc_ref(new_wrap_ctx->under_vol_id);
   H5VLget_wrap_ctx(o->under_object, o->under_vol_id,
@@ -70,23 +69,23 @@ herr_t H5VL_as_rpc_get_wrap_ctx(const void* obj, void** wrap_ctx)
   return 0;
 }
 
-void* H5VL_as_rpc_wrap_object(void* obj, H5I_type_t obj_type, void* _wrap_ctx)
+void* H5VL_as_rpc_wrap_object(void* obj, H5I_type_t obj_type, void* wrap_ctx)
 {
-  auto wrap_ctx = (H5VL_as_rpc_ctx_t*)_wrap_ctx;
+  auto ctx = static_cast<H5VL_as_rpc_ctx_t*>(wrap_ctx);
   H5VL_as_rpc_t* new_obj = nullptr;
   void* under;
 
-  under = H5VLwrap_object(obj, obj_type, wrap_ctx->under_vol_id,
-                          wrap_ctx->under_wrap_ctx);
+  under =
+      H5VLwrap_object(obj, obj_type, ctx->under_vol_id, ctx->under_wrap_ctx);
   if (under)
-    new_obj = H5VL_as_rpc_new_obj(under, wrap_ctx->under_vol_id);
+    new_obj = H5VL_as_rpc_new_obj(under, ctx->under_vol_id);
 
   return new_obj;
 }
 
 void* H5VL_as_rpc_unwrap_object(void* obj)
 {
-  auto o = (H5VL_as_rpc_t*)obj;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
   void* under = H5VLunwrap_object(o->under_object, o->under_vol_id);
 
   if (under)
@@ -95,17 +94,17 @@ void* H5VL_as_rpc_unwrap_object(void* obj)
   return under;
 }
 
-herr_t H5VL_as_rpc_free_wrap_ctx(void* _wrap_ctx)
+herr_t H5VL_as_rpc_free_wrap_ctx(void* wrap_ctx)
 {
-  auto wrap_ctx = (H5VL_as_rpc_ctx_t*)_wrap_ctx;
+  auto ctx = static_cast<H5VL_as_rpc_ctx_t*>(wrap_ctx);
   hid_t err;
 
   err = H5Eget_current_stack();
-  if (wrap_ctx->under_wrap_ctx)
-    H5VLfree_wrap_ctx(wrap_ctx->under_wrap_ctx, wrap_ctx->under_vol_id);
-  H5Idec_ref(wrap_ctx->under_vol_id);
+  if (ctx->under_wrap_ctx)
+    H5VLfree_wrap_ctx(ctx->under_wrap_ctx, ctx->under_vol_id);
+  H5Idec_ref(ctx->under_vol_id);
   H5Eset_current_stack(err);
-  free(wrap_ctx);
+  delete ctx;
 
   return 0;
 }
