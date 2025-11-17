1afe86316e608c1dd371125d546fc16f6588a355
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-11-17 16:44:44 +0100

    introduce log function

--- plugin/H5VLpassthru_ext.cpp
+++ plugin/H5VLpassthru_ext.cpp
@@ -26,6 +26,7 @@
 #include "info.hpp"
 #include "init.hpp"
 #include "link.hpp"
+#include "log.hpp"
 #include "object.hpp"
 #include "optional.hpp"
 #include "request.hpp"
@@ -36,7 +37,6 @@
 #include <H5PLextern.h>
 #include <H5VLpublic.h>
 #include <hdf5.h>
-#include <print>
 
 namespace
 {
@@ -164,9 +164,7 @@ herr_t H5VL_as_rpc_introspect_get_conn_cls(void* obj, H5VL_get_conn_lvl_t lvl,
   auto o = static_cast<H5VL_as_rpc_t*>(obj);
   herr_t ret_value;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  std::print(stderr, "------- EXT PASS THROUGH VOL INTROSPECT GetConnCls\n");
-#endif
+  log_msg("INTROSPECT GetConnCls\n");
 
   /* Check for querying this connector's class */
   if (H5VL_GET_CONN_LVL_CURR == lvl)
@@ -186,9 +184,7 @@ herr_t H5VL_as_rpc_introspect_get_cap_flags(const void* info,
 {
   auto info_cast = static_cast<const H5VL_as_rpc_info_t*>(info);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  std::print(stderr, "------- EXT PASS THROUGH VOL INTROSPECT GetCapFlags\n");
-#endif
+  log_msg("INTROSPECT GetCapFlags\n");
 
   /* Invoke the query on the underlying VOL connector */
   herr_t ret_value = H5VLintrospect_get_cap_flags(
@@ -206,9 +202,7 @@ herr_t H5VL_as_rpc_introspect_opt_query(void* obj, H5VL_subclass_t cls,
 {
   auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  std::print(stderr, "------- EXT PASS THROUGH VOL INTROSPECT OptQuery\n");
-#endif
+  log_msg("INTROSPECT OptQuery\n");
 
   herr_t ret_value = H5VLintrospect_opt_query(o->under_object, o->under_vol_id,
                                               cls, op_type, flags);
