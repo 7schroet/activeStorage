f45b3d1e0329f950bdf119fd1f1963e75d742af3
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-11-17 17:37:16 +0100

    refactor file

--- plugin/components/file.cpp
+++ plugin/components/file.cpp
@@ -19,101 +19,93 @@
 
 #include "file.hpp"
 #include "info.hpp"
+#include "log.hpp"
 #include "utils.hpp"
 #include <cstring>
 
 void* H5VL_as_rpc_file_create(const char* name, unsigned flags, hid_t fcpl_id,
                               hid_t fapl_id, hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_info_t* info;
-  H5VL_as_rpc_t* file;
+  void* info = nullptr;
+  H5VL_as_rpc_t* file = nullptr;
   hid_t under_fapl_id;
-  void* under;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL FILE Create\n");
-#endif
+  log_msg("FILE Create\n");
 
   /* Get copy of our VOL info from FAPL */
-  H5Pget_vol_info(fapl_id, (void**)&info);
+  H5Pget_vol_info(fapl_id, &info);
 
   if (!info)
-    return NULL;
+    return nullptr;
 
+  auto info_cast = static_cast<H5VL_as_rpc_info_t*>(info);
   under_fapl_id = H5Pcopy(fapl_id);
 
   /* Set the VOL ID and info for the underlying FAPL */
-  H5Pset_vol(under_fapl_id, info->under_vol_id, info->under_vol_info);
+  H5Pset_vol(under_fapl_id, info_cast->under_vol_id, info_cast->under_vol_info);
 
   /* Open the file with the underlying VOL connector */
-  under = H5VLfile_create(name, flags, fcpl_id, under_fapl_id, dxpl_id, req);
+  void* under =
+      H5VLfile_create(name, flags, fcpl_id, under_fapl_id, dxpl_id, req);
   if (under)
   {
-    file = H5VL_as_rpc_t_new_obj(under, info->under_vol_id);
+    file = H5VL_as_rpc_t_new_obj(under, info_cast->under_vol_id);
 
     if (req && *req)
-      *req = H5VL_as_rpc_t_new_obj(*req, info->under_vol_id);
+      *req = H5VL_as_rpc_t_new_obj(*req, info_cast->under_vol_id);
   }
-  else
-    file = NULL;
 
   H5Pclose(under_fapl_id);
 
   H5VL_as_rpc_info_free(info);
 
-  return (void*)file;
+  return file;
 }
 
 void* H5VL_as_rpc_file_open(const char* name, unsigned flags, hid_t fapl_id,
                             hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_info_t* info;
-  H5VL_as_rpc_t* file;
+  void* info = nullptr;
+  H5VL_as_rpc_t* file = nullptr;
   hid_t under_fapl_id;
-  void* under;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL FILE Open\n");
-#endif
+  log_msg("FILE Open\n");
 
-  H5Pget_vol_info(fapl_id, (void**)&info);
+  H5Pget_vol_info(fapl_id, &info);
 
   if (!info)
-    return NULL;
+    return nullptr;
+
+  auto info_cast = static_cast<H5VL_as_rpc_info_t*>(info);
 
   under_fapl_id = H5Pcopy(fapl_id);
 
-  H5Pset_vol(under_fapl_id, info->under_vol_id, info->under_vol_info);
+  H5Pset_vol(under_fapl_id, info_cast->under_vol_id, info_cast->under_vol_info);
 
-  under = H5VLfile_open(name, flags, under_fapl_id, dxpl_id, req);
+  void* under = H5VLfile_open(name, flags, under_fapl_id, dxpl_id, req);
   if (under)
   {
-    file = H5VL_as_rpc_t_new_obj(under, info->under_vol_id);
+    file = H5VL_as_rpc_t_new_obj(under, info_cast->under_vol_id);
 
     if (req && *req)
-      *req = H5VL_as_rpc_t_new_obj(*req, info->under_vol_id);
+      *req = H5VL_as_rpc_t_new_obj(*req, info_cast->under_vol_id);
   }
-  else
-    file = NULL;
 
   H5Pclose(under_fapl_id);
 
   H5VL_as_rpc_info_free(info);
 
-  return (void*)file;
+  return file;
 }
 
-herr_t H5VL_as_rpc_file_get(void* file, H5VL_file_get_args_t* args,
+herr_t H5VL_as_rpc_file_get(void* obj, H5VL_file_get_args_t* args,
                             hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)file;
-  herr_t ret_value;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL FILE Get\n");
-#endif
+  log_msg("FILE Get\n");
 
-  ret_value =
+  herr_t ret_value =
       H5VLfile_get(o->under_object, o->under_vol_id, args, dxpl_id, req);
 
   if (req && *req)
@@ -122,45 +114,39 @@ herr_t H5VL_as_rpc_file_get(void* file, H5VL_file_get_args_t* args,
   return ret_value;
 }
 
-herr_t H5VL_as_rpc_file_specific(void* file, H5VL_file_specific_args_t* args,
+herr_t H5VL_as_rpc_file_specific(void* obj, H5VL_file_specific_args_t* args,
                                  hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)file;
-  H5VL_as_rpc_t* new_o;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
+  H5VL_as_rpc_t* new_o = nullptr;
   H5VL_file_specific_args_t my_args;
   H5VL_file_specific_args_t* new_args;
   H5VL_as_rpc_info_t* info;
   hid_t under_vol_id = -1;
-  herr_t ret_value;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL FILE Specific\n");
-#endif
+  log_msg("FILE Specific\n");
 
   if (args->op_type == H5VL_FILE_IS_ACCESSIBLE)
   {
     memcpy(&my_args, args, sizeof(my_args));
 
-    /* Set up the new FAPL for the updated arguments */
-
+    // Set up the new FAPL for the updated arguments
     H5Pget_vol_info(args->args.is_accessible.fapl_id, (void**)&info);
 
     if (!info)
       return (-1);
 
-    /* Keep the correct underlying VOL ID for later */
+    // Keep the correct underlying VOL ID for later
     under_vol_id = info->under_vol_id;
 
     my_args.args.is_accessible.fapl_id =
         H5Pcopy(args->args.is_accessible.fapl_id);
 
-    /* Set the VOL ID and info for the underlying FAPL */
+    // Set the VOL ID and info for the underlying FAPL
     H5Pset_vol(my_args.args.is_accessible.fapl_id, info->under_vol_id,
                info->under_vol_info);
 
     new_args = &my_args;
-
-    new_o = NULL;
   }
   else if (args->op_type == H5VL_FILE_DELETE)
   {
@@ -179,19 +165,16 @@ herr_t H5VL_as_rpc_file_specific(void* file, H5VL_file_specific_args_t* args,
                info->under_vol_info);
 
     new_args = &my_args;
-
-    new_o = NULL;
   }
   else
   {
     under_vol_id = o->under_vol_id;
-
     new_args = args;
-
     new_o = (H5VL_as_rpc_t*)o->under_object;
   }
 
-  ret_value = H5VLfile_specific(new_o, under_vol_id, new_args, dxpl_id, req);
+  herr_t ret_value =
+      H5VLfile_specific(new_o, under_vol_id, new_args, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, under_vol_id);
@@ -208,7 +191,7 @@ herr_t H5VL_as_rpc_file_specific(void* file, H5VL_file_specific_args_t* args,
   }
   else if (args->op_type == H5VL_FILE_REOPEN)
   {
-    /* Wrap reopened file struct pointer, if we reopened one */
+    // Wrap reopened file struct pointer, if we reopened one
     if (ret_value >= 0 && args->args.reopen.file)
       *args->args.reopen.file =
           H5VL_as_rpc_t_new_obj(*args->args.reopen.file, o->under_vol_id);
@@ -217,17 +200,14 @@ herr_t H5VL_as_rpc_file_specific(void* file, H5VL_file_specific_args_t* args,
   return ret_value;
 }
 
-herr_t H5VL_as_rpc_file_optional(void* file, H5VL_optional_args_t* args,
+herr_t H5VL_as_rpc_file_optional(void* obj, H5VL_optional_args_t* args,
                                  hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)file;
-  herr_t ret_value;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL File Optional\n");
-#endif
+  log_msg("File Optional\n");
 
-  ret_value =
+  herr_t ret_value =
       H5VLfile_optional(o->under_object, o->under_vol_id, args, dxpl_id, req);
 
   if (req && *req)
@@ -236,16 +216,14 @@ herr_t H5VL_as_rpc_file_optional(void* file, H5VL_optional_args_t* args,
   return ret_value;
 }
 
-herr_t H5VL_as_rpc_file_close(void* file, hid_t dxpl_id, void** req)
+herr_t H5VL_as_rpc_file_close(void* obj, hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)file;
-  herr_t ret_value;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL FILE Close\n");
-#endif
+  log_msg("FILE Close\n");
 
-  ret_value = H5VLfile_close(o->under_object, o->under_vol_id, dxpl_id, req);
+  herr_t ret_value =
+      H5VLfile_close(o->under_object, o->under_vol_id, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, o->under_vol_id);
--- plugin/include/file.hpp
+++ plugin/include/file.hpp
@@ -24,14 +24,19 @@
 
 void* H5VL_as_rpc_file_create(const char* name, unsigned flags, hid_t fcpl_id,
                               hid_t fapl_id, hid_t dxpl_id, void** req);
+
 void* H5VL_as_rpc_file_open(const char* name, unsigned flags, hid_t fapl_id,
                             hid_t dxpl_id, void** req);
+
 herr_t H5VL_as_rpc_file_get(void* file, H5VL_file_get_args_t* args,
                             hid_t dxpl_id, void** req);
+
 herr_t H5VL_as_rpc_file_specific(void* file, H5VL_file_specific_args_t* args,
                                  hid_t dxpl_id, void** req);
+
 herr_t H5VL_as_rpc_file_optional(void* file, H5VL_optional_args_t* args,
                                  hid_t dxpl_id, void** req);
+
 herr_t H5VL_as_rpc_file_close(void* file, hid_t dxpl_id, void** req);
 
 #endif
