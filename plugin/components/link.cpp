6b9a896c7af77030330767ba2642e350bce4da6e
Niclas Schroeter <niclas.schroeter@googlemail.com>
2025-11-17 18:13:06 +0100

    refactor link

--- plugin/components/link.cpp
+++ plugin/components/link.cpp
@@ -18,8 +18,8 @@
  */
 
 #include "link.hpp"
+#include "log.hpp"
 #include "utils.hpp"
-#include <cassert>
 #include <cstring>
 
 herr_t H5VL_as_rpc_link_create(H5VL_link_create_args_t* args, void* obj,
@@ -29,36 +29,33 @@ herr_t H5VL_as_rpc_link_create(H5VL_link_create_args_t* args, void* obj,
 {
   H5VL_link_create_args_t my_args;
   H5VL_link_create_args_t* new_args;
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)obj;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
   hid_t under_vol_id = -1;
-  herr_t ret_value;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL LINK Create\n");
-#endif
+  log_msg("LINK Create\n");
 
-  /* Try to retrieve the "under" VOL id */
+  // Try to retrieve the "under" VOL id
   if (o)
     under_vol_id = o->under_vol_id;
 
-  /* Fix up the link target object for hard link creation */
+  // Fix up the link target object for hard link creation
   if (H5VL_LINK_CREATE_HARD == args->op_type)
   {
-    /* If it's a non-NULL pointer, find the 'under object' and re-set the args
-     */
+    // If it's a non-NULL pointer, find the 'under object' and reset the args
     if (args->args.hard.curr_obj)
     {
       memcpy(&my_args, args, sizeof(my_args));
 
-      /* Check if we still need the "under" VOL ID */
+      // Check if we still need the "under" VOL ID
       if (under_vol_id < 0)
-        under_vol_id = ((H5VL_as_rpc_t*)args->args.hard.curr_obj)->under_vol_id;
+        under_vol_id =
+            static_cast<H5VL_as_rpc_t*>(args->args.hard.curr_obj)->under_vol_id;
 
-      /* Set the object for the link target */
+      // Set the object for the link target
       my_args.args.hard.curr_obj =
-          ((H5VL_as_rpc_t*)args->args.hard.curr_obj)->under_object;
+          static_cast<H5VL_as_rpc_t*>(args->args.hard.curr_obj)->under_object;
 
-      /* Set argument pointer to modified parameters */
+      // Set argument pointer to modified parameters
       new_args = &my_args;
     }
     else
@@ -67,9 +64,9 @@ herr_t H5VL_as_rpc_link_create(H5VL_link_create_args_t* args, void* obj,
   else
     new_args = args;
 
-  /* Re-issue 'link create' call, possibly using the unwrapped pieces */
-  ret_value =
-      H5VLlink_create(new_args, (o ? o->under_object : NULL), loc_params,
+  // Re-issue 'link create' call, possibly using the unwrapped pieces
+  herr_t ret_value =
+      H5VLlink_create(new_args, (o ? o->under_object : nullptr), loc_params,
                       under_vol_id, lcpl_id, lapl_id, dxpl_id, req);
 
   if (req && *req)
@@ -83,24 +80,21 @@ H5VL_as_rpc_link_copy(void* src_obj, const H5VL_loc_params_t* loc_params1,
                       void* dst_obj, const H5VL_loc_params_t* loc_params2,
                       hid_t lcpl_id, hid_t lapl_id, hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_t* o_src = (H5VL_as_rpc_t*)src_obj;
-  H5VL_as_rpc_t* o_dst = (H5VL_as_rpc_t*)dst_obj;
+  auto o_src = static_cast<H5VL_as_rpc_t*>(src_obj);
+  auto o_dst = static_cast<H5VL_as_rpc_t*>(dst_obj);
   hid_t under_vol_id = -1;
-  herr_t ret_value;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL LINK Copy\n");
-#endif
+  log_msg("LINK Copy\n");
 
   if (o_src)
     under_vol_id = o_src->under_vol_id;
   else if (o_dst)
     under_vol_id = o_dst->under_vol_id;
-  assert(under_vol_id > 0);
 
-  ret_value = H5VLlink_copy((o_src ? o_src->under_object : NULL), loc_params1,
-                            (o_dst ? o_dst->under_object : NULL), loc_params2,
-                            under_vol_id, lcpl_id, lapl_id, dxpl_id, req);
+  herr_t ret_value =
+      H5VLlink_copy((o_src ? o_src->under_object : nullptr), loc_params1,
+                    (o_dst ? o_dst->under_object : nullptr), loc_params2,
+                    under_vol_id, lcpl_id, lapl_id, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, under_vol_id);
@@ -113,25 +107,22 @@ H5VL_as_rpc_link_move(void* src_obj, const H5VL_loc_params_t* loc_params1,
                       void* dst_obj, const H5VL_loc_params_t* loc_params2,
                       hid_t lcpl_id, hid_t lapl_id, hid_t dxpl_id, void** req)
 {
-  H5VL_as_rpc_t* o_src = (H5VL_as_rpc_t*)src_obj;
-  H5VL_as_rpc_t* o_dst = (H5VL_as_rpc_t*)dst_obj;
+  auto o_src = static_cast<H5VL_as_rpc_t*>(src_obj);
+  auto o_dst = static_cast<H5VL_as_rpc_t*>(dst_obj);
   hid_t under_vol_id = -1;
-  herr_t ret_value;
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL LINK Move\n");
-#endif
+  log_msg("LINK Move\n");
 
-  /* Retrieve the "under" VOL id */
+  // Retrieve the "under" VOL id
   if (o_src)
     under_vol_id = o_src->under_vol_id;
   else if (o_dst)
     under_vol_id = o_dst->under_vol_id;
-  assert(under_vol_id > 0);
 
-  ret_value = H5VLlink_move((o_src ? o_src->under_object : NULL), loc_params1,
-                            (o_dst ? o_dst->under_object : NULL), loc_params2,
-                            under_vol_id, lcpl_id, lapl_id, dxpl_id, req);
+  herr_t ret_value =
+      H5VLlink_move((o_src ? o_src->under_object : nullptr), loc_params1,
+                    (o_dst ? o_dst->under_object : nullptr), loc_params2,
+                    under_vol_id, lcpl_id, lapl_id, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, under_vol_id);
@@ -143,15 +134,12 @@ herr_t H5VL_as_rpc_link_get(void* obj, const H5VL_loc_params_t* loc_params,
                             H5VL_link_get_args_t* args, hid_t dxpl_id,
                             void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)obj;
-  herr_t ret_value;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL LINK Get\n");
-#endif
+  log_msg("LINK Get\n");
 
-  ret_value = H5VLlink_get(o->under_object, loc_params, o->under_vol_id, args,
-                           dxpl_id, req);
+  herr_t ret_value = H5VLlink_get(o->under_object, loc_params, o->under_vol_id,
+                                  args, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, o->under_vol_id);
@@ -163,15 +151,12 @@ herr_t H5VL_as_rpc_link_specific(void* obj, const H5VL_loc_params_t* loc_params,
                                  H5VL_link_specific_args_t* args, hid_t dxpl_id,
                                  void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)obj;
-  herr_t ret_value;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL LINK Specific\n");
-#endif
+  log_msg("LINK Specific\n");
 
-  ret_value = H5VLlink_specific(o->under_object, loc_params, o->under_vol_id,
-                                args, dxpl_id, req);
+  herr_t ret_value = H5VLlink_specific(o->under_object, loc_params,
+                                       o->under_vol_id, args, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, o->under_vol_id);
@@ -183,15 +168,12 @@ herr_t H5VL_as_rpc_link_optional(void* obj, const H5VL_loc_params_t* loc_params,
                                  H5VL_optional_args_t* args, hid_t dxpl_id,
                                  void** req)
 {
-  H5VL_as_rpc_t* o = (H5VL_as_rpc_t*)obj;
-  herr_t ret_value;
+  auto o = static_cast<H5VL_as_rpc_t*>(obj);
 
-#ifdef ENABLE_EXT_PASSTHRU_LOGGING
-  printf("------- EXT PASS THROUGH VOL LINK Optional\n");
-#endif
+  log_msg("LINK Optional\n");
 
-  ret_value = H5VLlink_optional(o->under_object, loc_params, o->under_vol_id,
-                                args, dxpl_id, req);
+  herr_t ret_value = H5VLlink_optional(o->under_object, loc_params,
+                                       o->under_vol_id, args, dxpl_id, req);
 
   if (req && *req)
     *req = H5VL_as_rpc_t_new_obj(*req, o->under_vol_id);
--- plugin/include/link.hpp
+++ plugin/include/link.hpp
@@ -26,20 +26,25 @@ herr_t H5VL_as_rpc_link_create(H5VL_link_create_args_t* args, void* obj,
                                const H5VL_loc_params_t* loc_params,
                                hid_t lcpl_id, hid_t lapl_id, hid_t dxpl_id,
                                void** req);
+
 herr_t
 H5VL_as_rpc_link_copy(void* src_obj, const H5VL_loc_params_t* loc_params1,
                       void* dst_obj, const H5VL_loc_params_t* loc_params2,
                       hid_t lcpl_id, hid_t lapl_id, hid_t dxpl_id, void** req);
+
 herr_t
 H5VL_as_rpc_link_move(void* src_obj, const H5VL_loc_params_t* loc_params1,
                       void* dst_obj, const H5VL_loc_params_t* loc_params2,
                       hid_t lcpl_id, hid_t lapl_id, hid_t dxpl_id, void** req);
+
 herr_t H5VL_as_rpc_link_get(void* obj, const H5VL_loc_params_t* loc_params,
                             H5VL_link_get_args_t* args, hid_t dxpl_id,
                             void** req);
+
 herr_t H5VL_as_rpc_link_specific(void* obj, const H5VL_loc_params_t* loc_params,
                                  H5VL_link_specific_args_t* args, hid_t dxpl_id,
                                  void** req);
+
 herr_t H5VL_as_rpc_link_optional(void* obj, const H5VL_loc_params_t* loc_params,
                                  H5VL_optional_args_t* args, hid_t dxpl_id,
                                  void** req);
