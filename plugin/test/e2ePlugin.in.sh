30db9c1b4c3cf153ead22ae0890dd3c382b8d0be
Niclas Schroeter <niclas.schroeter@googlemail.com>
2026-03-03 10:25:34 +0100

    add max kernel to plugin test

--- plugin/test/e2ePlugin.in.sh
+++ plugin/test/e2ePlugin.in.sh
@@ -4,26 +4,30 @@
 # $2: the h5 writer executable
 # $3: the name of the config file
 
-set -ue
+set -uex
 
-SERVER_EXE=$1
-H5_WRITER_EXE=$2
-CONFIG_NAME=$3
+server_exe=$1
+h5_writer_exe=$2
+config_name=$3
 
-ADDRESS_FILE=e2ePluginAddress
-PORT=8081
-H5FILE="@CMAKE_CURRENT_BINARY_DIR@/dummyWrite.h5"
-RESULT="@CMAKE_CURRENT_BINARY_DIR@/outmean.h5"
+address_file=e2ePluginAddress
+port=8081
+h5file="@CMAKE_CURRENT_BINARY_DIR@/dummyWrite.h5"
+
+result_mean="@CMAKE_CURRENT_BINARY_DIR@/outmean.h5"
+result_max="@CMAKE_CURRENT_BINARY_DIR@/outmax.h5"
+result_ref_mean="@CMAKE_CURRENT_SOURCE_DIR@/e2ePluginMeanRef.h5"
+result_ref_max="@CMAKE_CURRENT_SOURCE_DIR@/e2ePluginMaxRef.h5"
 
 export HDF5_USE_FILE_LOCKING=FALSE
 
-$SERVER_EXE --addressfile $ADDRESS_FILE --port $PORT &
+$server_exe --addressfile $address_file --port $port &
 PID=$!
 
 sleep 1
 
 function cleanup(){
-  rm -f "$RESULT" "$ADDRESS_FILE" "$H5FILE"
+  rm -f "$result_mean" "$result_max" "$address_file" "$h5file"
   kill $PID
 }
 trap cleanup EXIT
@@ -31,8 +35,8 @@ trap cleanup EXIT
 HDF5_PLUGIN_PATH=$(realpath "@CMAKE_CURRENT_BINARY_DIR@/.."); export HDF5_PLUGIN_PATH
 export HDF5_VOL_CONNECTOR="as-rpc-hdf5 under_vol=0;under_info={};"
 
-export AS_RPC_SERVER_ADDRESS=$PWD/${ADDRESS_FILE}
-export AS_RPC_OPERATIONS="@CMAKE_CURRENT_BINARY_DIR@/$CONFIG_NAME"
+export AS_RPC_SERVER_ADDRESS=$PWD/${address_file}
+export AS_RPC_OPERATIONS="@CMAKE_CURRENT_BINARY_DIR@/$config_name"
 
 (
   for _ in $(seq 1 2); do
@@ -41,21 +45,24 @@ export AS_RPC_OPERATIONS="@CMAKE_CURRENT_BINARY_DIR@/$CONFIG_NAME"
     done
     sleep 0.1
   done
-) | $H5_WRITER_EXE
+) | $h5_writer_exe
 
 # see e2eMean.in.sh for explanation
 set +e
 unset HDF5_VOL_CONNECTOR
-result_ref="@CMAKE_CURRENT_SOURCE_DIR@/e2ePluginRef.h5"
-h5diff_result=$(@HDF5_DIFF_EXECUTABLE@ -d 0.5 -c "$RESULT" "$result_ref")
-if [[ "$h5diff_result" || $? != 0 ]]; then
-  echo "Failure while comparing to $result_ref:"
-  echo "$h5diff_result"
+for op in "max" "mean"; do
+  expected="result_ref_${op}"
+  actual="result_${op}"
+  h5diff_result=$(@HDF5_DIFF_EXECUTABLE@ -c "${!actual}" "${!expected}")
+  if [[ "$h5diff_result" || $? != 0 ]]; then
+    echo "Failure while comparing to ${!expected}:"
+    echo "$h5diff_result"
 
-  copy_on_fail="./failure.h5"
-  echo "Copying failed file to $(realpath $copy_on_fail)"
-  cp "$RESULT" "$copy_on_fail"
+    copy_on_fail="./failure.h5"
+    echo "Copying failed file to $(realpath $copy_on_fail)"
+    cp "${!actual}" "$copy_on_fail"
 
-  exit 1
-fi
+    exit 1
+  fi
+done
 set -e
