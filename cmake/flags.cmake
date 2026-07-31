d60b744291d76995efe54cf371f62eb94a9a6b7a
Niclas Schroeter <niclas.schroeter@googlemail.com>
2026-07-31 13:52:26 +0200

    properly check for sanitizer flags

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -36,4 +36,49 @@ macro(populate_flags flag-interface)
       )
     endif()
   endforeach()
+
+  if(ENABLE_SANITIZER)
+    set(sanitizer_flag -fsanitize=address,leak,undefined)
+    if(DEFINED CMAKE_REQUIRED_LINK_OPTIONS AND CMAKE_REQUIRED_LINK_OPTIONS)
+      set(prev_link_options ${CMAKE_REQUIRED_LINK_OPTIONS})
+    else()
+      set(prev_link_options)
+    endif()
+
+    set(CMAKE_REQUIRED_LINK_OPTIONS ${sanitizer_flag})
+    check_cxx_compiler_flag(
+      ${sanitizer_flag}
+      SANITIZER_CXX_WORKS
+    )
+    check_c_compiler_flag(
+      ${sanitizer_flag}
+      SANITIZER_C_WORKS
+    )
+    if(SANITIZER_CXX_WORKS)
+      target_compile_options(
+        ${flag-interface}
+        INTERFACE
+          "$<$<COMPILE_LANGUAGE:CXX>:${sanitizer_flag}>"
+      )
+      target_link_options(
+        ${flag-interface}
+        INTERFACE
+          "$<$<COMPILE_LANGUAGE:CXX>:${sanitizer_flag}>"
+      )
+    endif()
+    if(SANITIZER_C_WORKS)
+      target_compile_options(
+        ${flag-interface}
+        INTERFACE
+          "$<$<COMPILE_LANGUAGE:C>:${sanitizer_flag}>"
+      )
+      target_link_options(
+        ${flag-interface}
+        INTERFACE
+          "$<$<COMPILE_LANGUAGE:C>:${sanitizer_flag}>"
+      )
+    endif()
+
+    set(CMAKE_REQUIRED_LINK_OPTIONS ${prev_link_options})
+  endif()
 endmacro()
